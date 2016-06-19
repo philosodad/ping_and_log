64a9b42b06e2dcfba4b7de5bcdf7b79fafb39da3
Paul Daigle <j.paul.daigle@gmail.com>
2016-06-19 13:01:31 -0400

    Modify tests to work with new supervision scheme

--- test/ping_and_log_test.exs
+++ test/ping_and_log_test.exs
@@ -2,14 +2,6 @@ defmodule PingAndLogTest do
   use ExUnit.Case
   doctest PingAndLog
 
-  setup do
-    PingAndLog.start
-    on_exit fn ->
-      PingAndLog.stop
-    end
-    :ok
-  end
-
   test "responds with Pong" do
     {:ok, response} = HTTPoison.get("localhost:#{Settings.port}/ping")
     assert response.status_code == 200
